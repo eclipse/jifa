01dd4f996bd9c4407f1fb78af3158a765b6be01b
denghui.ddh <denghui.ddh@alibaba-inc.com>
2021-12-17 14:32:54 +0800

    feat: make api port configurable

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     port: 8089,
     proxy: {
       '/jifa-api': {
-        target: 'http://127.0.0.1:8102'
+        target: 'http://127.0.0.1:' + (process.env.JIFA_API_PORT || 8012)
       },
     }
   },
