b243fdd5fd617a9ffb3ae0d638395f12325142cb
yifeng.jyf <yifeng.jyf@alibaba-inc.com>
2022-10-11 16:14:39 +0800

    fix: mark invalid problem

--- backend/gc-log-analyzer/src/main/java/org/eclipse/jifa/gclog/parser/GCLogAnalyzer.java
+++ backend/gc-log-analyzer/src/main/java/org/eclipse/jifa/gclog/parser/GCLogAnalyzer.java
@@ -22,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class GCLogAnalyzer {
 
@@ -37,26 +38,47 @@ public class GCLogAnalyzer {
     }
 
     public GCModel parse() throws Exception {
-        try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
             listener.beginTask("Paring " + file.getName(), 1000);
-            listener.sendUserMessage(ProgressListener.Level.INFO, "Reading gc log file.", null);
+            listener.sendUserMessage(ProgressListener.Level.INFO, "Deciding gc log format.", null);
+
+            // decide log format
             GCLogParserFactory logParserFactory = new GCLogParserFactory();
             br.mark(GCLogParserFactory.MAX_ATTEMPT_LINE * MAX_SINGLE_LINE_LENGTH);
             GCLogParser parser = logParserFactory.getParser(br);
-            br.reset();
-            // first read original info from log file
+            listener.worked(100);
+
+            try {
+                br.reset();
+            } catch (IOException e) {
+                // Recreate stream in case mark invalid. This is unlikely but possible when the log
+                // contains undesired characters
+                br.close();
+                br = new BufferedReader(new FileReader(file));
+            }
+
+            // read original info from log file
+            listener.sendUserMessage(ProgressListener.Level.INFO, "Parsing gc log file.", null);
             GCModel model = parser.parse(br);
             if (model.isEmpty()) {
-                throw new JifaException("Fail to parse gclog. Is this really a gc log?\"");
+                throw new JifaException("Fail to parse gc log. Is this really a gc log?");
             }
-            // then calculate derived info for query from original info
             listener.worked(500);
+
+            // calculate derived info for query from original info
             listener.sendUserMessage(ProgressListener.Level.INFO, "Calculating information from original data.", null);
             model.calculateDerivedInfo(listener);
+
             return model;
         } catch (Exception e) {
             LOGGER.info("fail to parse gclog {}: {}", file.getName(), e.getMessage());
             throw e;
+        } finally {
+            if (br != null) {
+                br.close();
+            }
         }
     }
 }
